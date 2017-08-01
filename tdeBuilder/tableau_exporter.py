ac6d92fee1dc3ed80114117712ab5b0c46d60736
a.pleskanovsky <a.pleskanovsky@corp.nekki.ru>
2017-08-01 17:23:17 +0300

    little logging

--- tdeBuilder/tableau_exporter.py
+++ tdeBuilder/tableau_exporter.py
@@ -5,5 +5,8 @@ def publish_tde(server, login, password, site, full_local_path, project_name, ds
     connection = TableauRestApiConnection(server=unicode(server), username=unicode(login), password=unicode(password), site_content_url=unicode(site))
     connection.signin()
     project_obj = connection.query_project(unicode(project_name))
+    print full_local_path
+    print ds_name
+    print project_name
     connection.publish_datasource(unicode(full_local_path), unicode(ds_name), project_obj)
 
